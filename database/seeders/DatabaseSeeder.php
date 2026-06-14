6f4c253bd4362c5b53000f0fb8d874a421253cc2
vamsiKrishna0315 <vamsi31095@gmail.com>
2026-06-14 14:53:31 +0530

    fix: added userSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,8 @@ final class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $this->call([
+            UsersSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+final class UsersSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('users')->updateOrInsert(
+            ['email' => 'admin@mail.com'],
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'user_mobile_no' => null,
+                'user_role' => 'admin',
+                'email_verified_at' => null,
+                'password' => '$2y$12$CcCo75ZF7OVukJuuL44djeDGX.yiNSRpf9D3gg0fN0956kAruYytq',
+                'address' => null,
+                'address_proof' => null,
+                'finger_print' => null,
+                'joining_date' => null,
+                'alternate_number' => null,
+                'dob' => null,
+                'salary' => null,
+                'store_id' => null,
+                'location' => null,
+                'store_lat' => null,
+                'store_lng' => null,
+                'status' => 1,
+                'remember_token' => null,
+                'reputation' => 0,
+                'created_at' => '2025-10-19 06:08:05',
+                'updated_at' => '2025-10-24 13:10:51',
+            ]
+        );
+    }
+}
