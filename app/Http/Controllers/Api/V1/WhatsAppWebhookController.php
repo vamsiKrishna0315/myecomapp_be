d73e226d62f12bae8c4f134101ad5dc765164ab3
vamsiKrishna0315 <vamsi31095@gmail.com>
2026-06-29 11:57:41 +0530

    fix : added meta query

--- app/Http/Controllers/Api/V1/WhatsAppWebhookController.php
+++ app/Http/Controllers/Api/V1/WhatsAppWebhookController.php
@@ -9,18 +9,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class WhatsAppWebhookController extends ResponseController
 {
+
     public function verify(Request $request): Response
     {
         $verifyToken = config('whatsapp.webhook_verify_token');
-        dd($request->query());
 
         if (
-            $request->query('hub.mode') !== 'subscribe' ||
-            $request->query('hub.verify_token') !== $verifyToken
+            $request->query('hub_mode') !== 'subscribe' ||
+            $request->query('hub_verify_token') !== $verifyToken
         ) {
             abort(403, 'Invalid verify token.');
         }
 
-      return response($request->query('hub.challenge'), 200);
+        return response($request->query('hub_challenge'), 200);
     }
 }
